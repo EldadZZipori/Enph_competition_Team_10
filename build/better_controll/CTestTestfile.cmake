dfa3b23de87790865dcf42ed753b2bbf945a51ac
EldadZZipori <eldad.z.zipori@gmail.com>
2023-03-20 18:59:35 -0700

    first commit

---
+++ build/better_controll/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/fizzer/ros_ws/src/better_controll
+# Build directory: /home/fizzer/ros_ws/build/better_controll
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

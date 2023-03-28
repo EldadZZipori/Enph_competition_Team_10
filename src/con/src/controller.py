7f861e21da1e30703101c10f3ded66b64fc676dc
EldadZZipori <eldad.z.zipori@gmail.com>
2023-03-27 17:43:09 -0700

    add controller.py code

--- src/con/src/controller.py
+++ src/con/src/controller.py
@@ -1,17 +1,64 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 
 import rospy
+import sys, select, termios, tty
 from geometry_msgs.msg import Twist
 
+def getch():
+    """
+    Function to read a single character from the keyboard
+    """
+    tty.setraw(sys.stdin.fileno())
+    select.select([sys.stdin], [], [], 0)
+    key = sys.stdin.read(1)
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
 
-rospy.init_node('topic_publisher')
-pub = rospy.Publisher('/R1/cmd_vel', Twist, 
-  queue_size=1)
-rate = rospy.Rate(2)
-move = Twist()
-move.linear.x = 0.5
-move.angular.z = 0.5
+if __name__=="__main__":
+    # Initialize the ROS node
+    rospy.init_node('teleop_keyboard')
 
-while not rospy.is_shutdown():
-   pub.publish(move)
-   rate.sleep()
+    # Create a publisher for the cmd_vel topic
+    pub = rospy.Publisher('/R1/cmd_vel', Twist, queue_size=10)
+
+    # Set the publishing rate
+    rate = rospy.Rate(10)
+
+    # Set the initial velocities to zero
+    twist = Twist()
+    twist.linear.x = 0.0
+    twist.angular.z = 0.0
+
+    # Save the terminal settings
+    settings = termios.tcgetattr(sys.stdin)
+
+    # Main loop
+    while not rospy.is_shutdown():
+        # Read a single character from the keyboard
+        key = getch()
+
+        # Check if any of the movement keys are pressed
+        if key == 'w':
+            twist.linear.x = 0.2
+            twist.angular.z = 0.0
+        elif key == 's':
+            twist.linear.x = -0.2
+            twist.angular.z = 0.0
+        elif key == 'a':
+            twist.linear.x = 0.0
+            twist.angular.z = 0.5
+        elif key == 'd':
+            twist.linear.x = 0.0
+            twist.angular.z = -0.5
+        else:
+            twist.linear.x = 0.0
+            twist.angular.z = 0.0
+
+        # Publish the twist message
+        pub.publish(twist)
+
+        # Sleep to maintain the publishing rate
+        rate.sleep()
+
+    # Restore the terminal settings
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
